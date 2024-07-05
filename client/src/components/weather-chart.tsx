ff6c6b8ee239b2768318ba74519f00b3675edc33
Sreejith <132558235+Sreejith-10@users.noreply.github.com>
2024-07-05 17:36:49 +0530

    main

--- client/src/components/weather-chart.tsx
+++ client/src/components/weather-chart.tsx
@@ -3,10 +3,14 @@ const WeatherChart = () => {
 		<div className="w-[80%] h-80 bg-orange-300/50 rounded-3xl grid grid-cols-5 p-5">
 			{Array(10)
 				.fill("")
-				.map((item, index) => (
-					<div className="flex flex-col items-center" key={index}>
-						<h2>Now</h2>
-						<div>icon</div>
+				.map((_, index) => (
+					<div
+						className="flex flex-col items-center justify-center gap-3 text-slate-50"
+						key={index}>
+						<h2 className="font-semibold text-lg">{index + 1} AM</h2>
+						<div>
+							<h2 className="font-semibold text-lg">22 &deg;</h2>
+						</div>
 					</div>
 				))}
 		</div>
--- client/src/components/weather-menu.tsx
+++ client/src/components/weather-menu.tsx
@@ -1,17 +1,23 @@
 const WeatherMenu = () => {
 	return (
-		<div className="w-full h-full bg-orange-300 rounded-3xl flex flex-col gap-5 p-10 items-center">
-			<div>Today</div>
+		<div className="w-full h-full bg-orange-200 rounded-3xl flex flex-col gap-5 p-10 items-center justify-evenly text-orange-600">
 			<div>
-				<div>icon</div>
-				<h1>32</h1>
+				<h1 className="font-bold text-[30px]">Today</h1>
 			</div>
-			<div>
-				<h1>Aluva</h1>
+			<div className="flex gap-5 items-center">
+				<div>
+					<img
+						src="/public/sun.png"
+						alt="icon"
+						className="size-40 text-orange-400"
+					/>
+				</div>
+				<h1 className="font-semibold text-[100px]">32 &deg;</h1>
 			</div>
-			<div>
-				<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit</p>
-			</div>
+			<h1 className="font-semibold text-2xl">Sunny</h1>
+			<h1 className="font-semibold text-2xl">New Delhi</h1>
+			<h2 className="font-semibold text-2xl">5 July 2024</h2>
+			<h2 className="font-semibold text-2xl">Feels like 30 | Sunset 18:20</h2>
 		</div>
 	);
 };
